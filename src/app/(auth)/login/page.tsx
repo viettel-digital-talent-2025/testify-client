c13934938dca88d8cf04b60ce007c3252b0429aa
Npeka <npkhang22@clc.fitus.edu.vn>
2025-05-19 03:24:56 +0700

    feat: add login page

---
+++ src/app/(auth)/login/page.tsx
@@ -0,0 +1,22 @@
+import { Card } from "antd";
+import {
+  FormTitle,
+  FormDescription,
+  FormFooter,
+} from "@/modules/auth/components/form/Form";
+import { LoginForm } from "@/modules/auth/components/form";
+
+export default function LoginPage() {
+  return (
+    <Card>
+      <FormTitle title="Sign in to Testify" />
+      <FormDescription description="Welcome back! Please enter your credentials." />
+      <LoginForm />
+      <FormFooter
+        description="Don't have an account?"
+        link="/register"
+        linkText="Register"
+      />
+    </Card>
+  );
+}
