4bed52ec62a5dfe3ab648cbec06ba84ad8a2ee29
Npeka <npkhang22@clc.fitus.edu.vn>
2025-05-19 03:29:46 +0700

    feat: add shared redux toolkit api config

---
+++ src/modules/shared/store/api/appApi.ts
@@ -0,0 +1,57 @@
+"use client";
+import {
+  BaseQueryFn,
+  createApi,
+  FetchArgs,
+  fetchBaseQuery,
+  FetchBaseQueryError,
+} from "@reduxjs/toolkit/query/react";
+import { RootState } from "@/modules/shared/store/store";
+import { setAccessToken, setSignOut } from "@/modules/auth/redux/authSlide";
+import { RefreshTokenResponse } from "@/modules/auth/types/auth";
+
+const baseQuery = fetchBaseQuery({
+  baseUrl: process.env.NEXT_PUBLIC_BACKEND_URL,
+  credentials: "include",
+  prepareHeaders: (headers, { getState }) => {
+    const state = getState() as RootState;
+    const accessToken = state.auth.accessToken;
+    if (accessToken) {
+      headers.set("Authorization", `Bearer ${accessToken}`);
+    }
+    headers.set("Content-Type", "application/json");
+    return headers;
+  },
+});
+
+const baseQueryWithReauth: BaseQueryFn<
+  string | FetchArgs,
+  unknown,
+  FetchBaseQueryError
+> = async (args, api, extraOptions) => {
+  let result = await baseQuery(args, api, extraOptions);
+
+  if (result.error && result.error.status === 401) {
+    const refreshResult = await baseQuery(
+      "/api/v1/auth/refresh-token",
+      api,
+      extraOptions,
+    );
+
+    if (refreshResult.data) {
+      const data = refreshResult.data as RefreshTokenResponse;
+      api.dispatch(setAccessToken(data.accessToken));
+      result = await baseQuery(args, api, extraOptions);
+    } else {
+      api.dispatch(setSignOut());
+    }
+  }
+  return result;
+};
+
+export const appApi = createApi({
+  reducerPath: "appApi",
+  baseQuery: baseQueryWithReauth,
+  endpoints: () => ({}),
+  tagTypes: ["Auth"],
+});
