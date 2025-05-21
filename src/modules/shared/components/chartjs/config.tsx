5f1aefa3a1522fa7697c368ab8dd5a1e013569f0
Npeka <npkhang22@clc.fitus.edu.vn>
2025-05-22 00:10:20 +0700

    feat: add chartjs config

---
+++ src/modules/shared/components/chartjs/config.tsx
@@ -0,0 +1,30 @@
+"use client";
+// lib/chart-config.ts
+import {
+  Chart as ChartJS,
+  CategoryScale,
+  LinearScale,
+  PointElement,
+  LineElement,
+  BarElement,
+  Title,
+  Tooltip,
+  Legend,
+  Filler,
+  Title as ChartTitle,
+} from "chart.js";
+
+ChartJS.register(
+  CategoryScale,
+  LinearScale,
+  PointElement,
+  LineElement,
+  BarElement,
+  Title,
+  Tooltip,
+  Legend,
+  Filler,
+  ChartTitle,
+);
+
+export default ChartJS;
