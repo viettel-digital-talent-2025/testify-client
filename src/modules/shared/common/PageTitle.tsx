274cab8a558aaf21776baa6c48910f803328dd98
Npeka <npkhang22@clc.fitus.edu.vn>
2025-05-22 00:08:07 +0700

    refactor: remove old file

--- src/modules/shared/common/PageTitle.tsx
+++
@@ -1,9 +0,0 @@
-import Title from 'antd/es/typography/Title';
-
-export default function PageTitle({ title }: { title: string }) {
-  return (
-    <Title level={2} style={{ margin: 0 }}>
-      {title}
-    </Title>
-  );
-}
--- src/modules/shared/common/index.ts
+++
@@ -1,4 +0,0 @@
-export { default as PageTitle } from './PageTitle';
-export { LayoutStatic, LayoutScroll } from './Layout';
-export { LoadingStatus, ErrorStatus, NoDataStatus } from './Status';
-export { ChartSkeleton, TableSkeleton, StatsSkeleton } from './Skeleton';
