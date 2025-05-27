d5be20d37c238b8039e6e833cdbedec4f67cf96d
Npeka <npkhang22@clc.fitus.edu.vn>
2025-05-28 02:49:32 +0700

    refactor: update theme provider

--- src/modules/shared/providers/ThemeProvider.tsx
+++ src/modules/shared/providers/ThemeProvider.tsx
@@ -1,18 +1,20 @@
-import { ConfigProvider, App } from "antd";
+"use client";
+import { ConfigProvider, App, theme as antdTheme } from "antd";
 import { AntdRegistry } from "@ant-design/nextjs-registry";
-import { NotificationProvider } from "@/modules/shared/contexts";
-import { colors } from "@/modules/shared/constants/colors";
-import "@/modules/shared/components/chartjs/config";
+import { NotificationProvider } from "@/shared/contexts";
+import { colors } from "@/shared/constants/colors";
+import "@/shared/components/chartjs/config";
 
 export const theme = {
+  algorithm: antdTheme.darkAlgorithm,
   token: {
     colorPrimary: colors.primary,
   },
   components: {
-    // Card: {
-    //   bodyPadding: 12,
-    //   headerPadding: 12,
-    // },
+    Card: {
+      bodyPadding: 12,
+      headerPadding: 12,
+    },
   },
 };
 
