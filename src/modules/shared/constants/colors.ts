af2894797ce4c52883f34aa16ce8bbb94c53871b
Npeka <npkhang22@clc.fitus.edu.vn>
2025-05-28 02:50:18 +0700

    feat: add constants colors

--- src/modules/shared/constants/colors.ts
+++ src/modules/shared/constants/colors.ts
@@ -1,4 +1,27 @@
 export const colors = {
-  primary: "#08457e",
-  secondary: "#f0f0f0",
+  primary: "#854eca",
+  secondary: "#9a9a9a",
+  primary2: "#24163a",
+  primary3: "#301c4d",
+
+  dark: "#171717",
+  background: "#1e1e1e",
+  border: "#4f4f4f",
+
+  text: "#e1e1e1",
+  textSecondary: "#b3b3b3",
+  textDescription: "#7e7e7e",
+  placeholder: "#4f4f4f",
+
+  blue: "#1890ff",
+  green: "#52c41a",
+  purple: "#722ed1",
+  orange: "#fa8c16",
+  gray: "#8c8c8c",
+  red: "#f5222d",
+  white: "#ffffff",
+  success: "#52c41a",
+  warning: "#faad14",
+  error: "#f5222d",
+  info: "#1890ff",
 };
